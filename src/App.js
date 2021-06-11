34a4112932517f3482b2b8b50bc0227d53b6c89b
developer-junaid <qjunaid623@gmail.com>
2021-06-11 06:58:12 +0500

    Added use ReactCountdown Package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useReactCountdown from "./hooks/use-react-countdown";
 
 function App() {
+  // Date in format (Month DD, YYYY hh:mm:ss)
+  const { days, hours, minutes, seconds } = useReactCountdown(
+    "July 22, 2021 00:00:00"
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My npm packages</h1>
+      <button>
+        Time: {days} {hours} {minutes} {seconds}
+      </button>
     </div>
   );
 }
---
+++ src/hooks/use-react-countdown.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
+const useReactCountdown = (date) => {
+  //COUNTDOWN
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date(date).getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        // Stop
+        clearInterval(interval.current);
+      } else {
+        // Update
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+  // Logic
+  return {
+    days: timerDays,
+    hours: timerHours,
+    minutes: timerMinutes,
+    seconds: timerSeconds,
+  };
+};
+
+export default useReactCountdown;
